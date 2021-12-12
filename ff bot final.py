c18966f03549abba05861f338a255c2e2f4e6f36
AwesomeDoggo7566 <96025284+AwesomeDoggo7566@users.noreply.github.com>
2021-12-12 16:24:41 -0500

    Add files via upload
    before you use it, you have to do these steps first!
    1. install python 3.7 (don't forget to check the add to path checkbox)
    2. open command prompt as administrator
    3. put in these following commands:
    pip install pyautogui
    pip install keyboard
    pip install pywin32
    pip install pynput
    after that, you should be good to go... the rest of the instructions will probably be in the video :`)

---
+++ ff bot final.py
@@ -0,0 +1,24 @@
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import random
+import win32api
+from pynput.keyboard import Key, Controller
+
+
+
+while keyboard.is_pressed('q') == False:
+    
+    if pyautogui.pixel(522, 214)[0] == 0:
+        keyboard.press("a")
+        keyboard.release("a")
+    if pyautogui.pixel(629, 214)[0] == 0:
+        keyboard.press("s")
+        keyboard.release("s")
+    if pyautogui.pixel(729, 214)[0] == 0:
+        keyboard.press('o')
+        keyboard.release("o")
+    if pyautogui.pixel(836, 214)[0] == 0:
+        keyboard.press('p')
+        keyboard.release("p")
